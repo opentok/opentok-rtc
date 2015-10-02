38e81783f97a1655d241969e4fc1501656d3f318
crdlc <crdlc@tid.es>
2015-10-02 12:05:51 +0200

    Added navigation to room from landing page

--- web/js/landingView.js
+++ web/js/landingView.js
@@ -14,7 +14,7 @@
     enterButton.disabled = !room.value.trim();
   };
 
-  var reset = function() {
+  var resetForm = function() {
     var fields = document.querySelectorAll('.join-modal input');
     Array.prototype.map.call(fields, function(field) {
       field.value = '';
@@ -34,7 +34,16 @@
           event.preventDefault();
           enterButton.removeEventListener('click', onEnterClicked);
           form.removeEventListener('input', onInput);
-          Modal.hide('.join-modal').then(reset);
+          Modal.hide('.join-modal').then(function() {
+            var base = window.location.href.replace('index.html', '');
+            var url = base.concat('room/', room.value);
+            var userName = document.getElementById('user').value.trim();
+            if (userName) {
+              url = url.concat('?userName=', userName);
+            }
+            resetForm();
+            window.location = url;
+          });
         });
       });
     });
