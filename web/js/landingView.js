92923c6d9daed1986a7683273bdf4e1c41818e0d
crdlc <crdlc@tid.es>
2015-10-30 13:08:50 +0100

    [PSE-75] Adapt landing page to the new visuals

--- web/js/landingView.js
+++ web/js/landingView.js
@@ -6,7 +6,7 @@
   var init = function() {
     enterButton = document.getElementById('enter');
     room = document.getElementById('room');
-
+    resetForm();
     addHandlers();
   };
 
@@ -15,7 +15,7 @@
   };
 
   var resetForm = function() {
-    var fields = document.querySelectorAll('.join-modal input');
+    var fields = document.querySelectorAll('form input');
     Array.prototype.map.call(fields, function(field) {
       field.value = '';
     });
@@ -23,29 +23,21 @@
   };
 
   var addHandlers = function() {
-    var join = document.getElementById('join');
+    var form = document.querySelector('form');
+    form.addEventListener('input', onInput);
 
-    join.addEventListener('click', function(e) {
-      Modal.show('.join-modal').then(function() {
-        var form = document.querySelector('.join-modal form');
-        form.addEventListener('input', onInput);
-
-        enterButton.addEventListener('click', function onEnterClicked(event) {
-          event.preventDefault();
-          enterButton.removeEventListener('click', onEnterClicked);
-          form.removeEventListener('input', onInput);
-          Modal.hide('.join-modal').then(function() {
-            var base = window.location.href.replace('index.html', '');
-            var url = base.concat('room/', room.value);
-            var userName = document.getElementById('user').value.trim();
-            if (userName) {
-              url = url.concat('?userName=', userName);
-            }
-            resetForm();
-            window.location = url;
-          });
-        });
-      });
+    enterButton.addEventListener('click', function onEnterClicked(event) {
+      event.preventDefault();
+      enterButton.removeEventListener('click', onEnterClicked);
+      form.removeEventListener('input', onInput);
+      var base = window.location.href.replace('index.html', '');
+      var url = base.concat('room/', room.value);
+      var userName = document.getElementById('user').value.trim();
+      if (userName) {
+        url = url.concat('?userName=', userName);
+      }
+      resetForm();
+      window.location = url;
     });
   };
 
