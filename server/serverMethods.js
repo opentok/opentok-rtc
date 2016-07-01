8ff5041cbd1358d62af018a8dad1168ac46abf0c
Antonio M. Amaya <amac@tid.es>
2016-07-01 11:35:48 +0200

    [PSE-528] Add var to the room template

--- server/serverMethods.js
+++ server/serverMethods.js
@@ -283,6 +283,7 @@ function ServerMethods(aLogLevel, aModules) {
     aRes.
       render((template ? template : tbConfig.defaultTemplate) + '.ejs',
              {
+               isWebRTCVersion: tbConfig.isWebRTCVersion,
                userName: userName || C.DEFAULT_USER_NAME,
                roomName: aReq.params.roomName,
                chromeExtensionId: tbConfig.chromeExtId,
