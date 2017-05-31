1f936b5113183eaefde12498de1167056ef65772
Mike Thomas <mthomas@tokbox.com>
2017-05-31 17:50:30 +0200

    fix style

--- server/configLoader.js
+++ server/configLoader.js
@@ -1,6 +1,3 @@
-'use strict';
-
-var fs = require('fs')
 var _ = require('lodash');
 var Utils = require('swagger-boilerplate').Utils;
 
@@ -12,36 +9,38 @@ var env = process.env;
 var exports = module.exports = {};
 
 class Config {
-    constructor(aConfigJson) {
-        this.configJson = aConfigJson;
-    }
+  constructor(aConfigJson) {
+    this.configJson = aConfigJson;
+  }
 
-    // aParam: Object with properties:
-    // envVar: Name of environment Variable to look for
-    // jsonPath: Path to value in object this.configJson
-    // defaultValue: Value to use if neither environment variable nor json found.
-    // parser: Function to return value as correct type i.e. parseInt.
-    // required: boolean, whether value can be undefined, or throw error.
-    get(aParam) {
-        var value = env[aParam.envVar] || _.get(this.configJson, aParam.jsonPath, aParam.defaultValue);
-        if (aParam.required && value === undefined)
-            throw new Error(`${aParam.envVar} must be supplied.`)
-        if (aParam.parser)
-            value = aParam.parser(value);
-        return value
+  // aParam: Object with properties:
+  // envVar: Name of environment Variable to look for
+  // jsonPath: Path to value in object this.configJson
+  // defaultValue: Value to use if neither environment variable nor json found.
+  // parser: Function to return value as correct type i.e. parseInt.
+  // required: boolean, whether value can be undefined, or throw error.
+  get(aParam) {
+    var value = env[aParam.envVar] || _.get(this.configJson, aParam.jsonPath, aParam.defaultValue);
+    if (aParam.required && value === undefined) {
+      throw new Error(`${aParam.envVar} must be supplied.`);
     }
+    if (aParam.parser) {
+      value = aParam.parser(value);
+    }
+    return value;
+  }
 }
 
 // Promise wrapper around reading config file.
 // Returns a Config instance
 exports.readConfigJson = () => {
-    var configPath = env.OTDEMO_CONFIG_FILE_PATH || defaultJsonConfigPath
-    return new Promise((resolve, reject) => {
-       try {
-          configPath = require.resolve(configPath);
-          readFile(configPath).then(resolve);
-       } catch (e) {
-          (e.code === 'MODULE_NOT_FOUND' && (resolve('{}') || true)) || reject(e);
-       }
-   }).then(data => new Config(JSON.parse(data)));
-}
+  var configPath = env.OTDEMO_CONFIG_FILE_PATH || defaultJsonConfigPath;
+  return new Promise((resolve, reject) => {
+    try {
+      configPath = require.resolve(configPath);
+      readFile(configPath).then(resolve);
+    } catch (e) {
+      (e.code === 'MODULE_NOT_FOUND' && (resolve('{}') || true)) || reject(e);
+    }
+  }).then(data => new Config(JSON.parse(data)));
+};
