daf0bd21cf5f4fca2af084af1bfed25847c21de6
Mike Thomas <mthomas@tokbox.com>
2017-09-28 16:00:14 +0200

    Distinguish chat user by connectionId

--- test/unit/chatView_spec.js
+++ test/unit/chatView_spec.js
@@ -220,7 +220,7 @@ describe('ChatView', () => {
 
          Chat._isVisible = true;
          this.spy(window, 'dispatchEvent');
-         data.sender = 'myself';
+         data.senderId = 'myConnectionId';
          window.dispatchEvent(new CustomEvent('chatController:incomingMessage',
                                            { detail: { data } }));
 
@@ -232,7 +232,7 @@ describe('ChatView', () => {
        sinon.test(function () {
          var chatContent = getChatContainer().querySelector('ul');
          var lengthBefore = chatContent.children.length;
-         data.sender = 'other';
+         data.senderId = 'other';
 
          Chat._isVisible = true;
          this.spy(window, 'dispatchEvent');
