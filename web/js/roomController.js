9784785910177534ae8359d5e00a0b87135f2c75
Emiliano Pelliccioni <emilianop11@gmail.com>
2020-03-19 19:49:51 -0300

    [ECO-4819] Options to set publisher res and subscriber framerate (#547)
    * Options to set publisher res and subscriber framerate
    
    * Remove subscriberReceivedFramerate

--- web/js/roomController.js
+++ web/js/roomController.js
@@ -38,7 +38,7 @@ RecordingsController, ScreenShareController, FeedbackController, PhoneNumberCont
     width: '100%',
     height: '100%',
     showControls: true,
-    resolution: '1280x720',
+    resolution: publisherResolution,
     style: {
       audioLevelDisplayMode: 'auto',
       buttonDisplayMode: 'off',
