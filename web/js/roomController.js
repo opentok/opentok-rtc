a8941c22f618c3814f7ceb43dd7ee77907fa7a06
SailuVejella <sailu.vejella@vonage.com>
2020-06-26 10:02:44 -0700

    Revert "load hot jar dependecy in js"
    This reverts commit 598e297096349e8bf4172c2f633ce5fad72cee65.
    fixing the issue in other way

--- web/js/roomController.js
+++ web/js/roomController.js
@@ -714,19 +714,6 @@ PhoneNumberController, ResizeSensor, maxUsersPerRoom */
     }
   };
 
-  function loadHotjar () {
-    if (enableFeedback && hotjarId && hotjarVersion) {
-      (function (h, o, t, j, a, r) {
-        h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
-        h._hjSettings = { hjid: hotjarId, hjsv: hotjarVersion };
-        a = o.getElementsByTagName('head')[0];
-        r = o.createElement('script'); r.async = 1;
-        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-        a.appendChild(r);
-      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-    }
-  }
-
   function showMobileShareUrl() {
     navigator.share({
       title: 'Invite Participant',
@@ -968,8 +955,7 @@ PhoneNumberController, ResizeSensor, maxUsersPerRoom */
         .catch(function (error) {
           debug.error('Error Connecting to room. ' + error.message);
         });
-    })
-  .then(loadHotjar());
+    });
   };
 
   var RoomController = {
