3399de505cc332ee8d18cb80884d162aa2890589
Emiliano Pelliccioni <emilianop11@gmail.com>
2020-10-22 16:56:33 -0300

    Remove leftovers

--- web/js/helpers/requests.js
+++ web/js/helpers/requests.js
@@ -103,20 +103,6 @@
     return sendXHR('DELETE', `${server}/archive/${id}`);
   }
 
-  function saveConnection(connection, sessionId) {
-    return sendXHR('POST', `${server}/saveConnection/`, JSON.stringify({
-      connection,
-      sessionId
-    }), 'application/json');
-  }
-
-  function deleteConnection(connection, sessionId) {
-    return sendXHR('POST', `${server}/deleteConnection/`, JSON.stringify({
-      connection,
-      sessionId
-    }), 'application/json');
-  }
-
   const Request = {
     getRoomInfo,
     getRoomRawInfo,
