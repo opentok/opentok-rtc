fc14f3318fd47d1508ac661fef839174cfffaf12
Emiliano Pelliccioni <emilianop11@gmail.com>
2020-10-27 16:44:51 -0300

    Config upgrade

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {}
+};
