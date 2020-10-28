843b7f3f77998fc07b433062fba48abe9ad5cd38
Emiliano Pelliccioni <emilianop11@gmail.com>
2020-10-28 20:28:26 -0300

    Update configLoader.js

--- server/configLoader.js
+++ server/configLoader.js
@@ -5,8 +5,6 @@ const readFile = Utils.promisify(require('fs').readFile);
 const defaultJsonConfigPath = require('./serverConstants').DEFAULT_JSON_CONFIG_PATH;
 
 const { env } = process;
-const exports = {};
-module.exports = {};
 
 class Config {
   constructor(aConfigJson) {
